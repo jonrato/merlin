1129b95d1285fa97315c04dda67bf425206bae3b
Jonatas <jonatas_ferreira@usp.br>
2021-05-21 15:29:50 -0300

    pages assinaturas

--- admindashboard/urls.py
+++ admindashboard/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from admindashboard import views
+from .views import CampoList
 urlpatterns = [
     path('admin-dashboard/', views.admin_dashboard, name=('admin-dashboard')),
     path('analitico-dashboard/', views.analitico_dashboard, name=('analitico-dashboard')),
     path('artigos-dashboard/', views.artigos_dashboard, name=('artigos-dashboard')),
     path('forum-dashboard/', views.forum_dashboard, name=('forum-dashboard')),
     path('produtos-dashboard/', views.produtos_dashboard, name=('produtos-dashboard')),
-    path('usuarios-dashboard/', views.usuarios_dashboard, name=('usuarios-dashboard')),
+    #path('usuarios-dashboard/', views.usuarios_dashboard, name=('usuarios-dashboard')),
+    path('usuarios-dashboard/', CampoList.as_view(), name=('campolist-dashboard')),
     path('vendas-dashboard/', views.vendas_dashboard, name=('vendas-dashboard')),
 
     
--- admindashboard/views.py
+++ admindashboard/views.py
@@ -1,5 +1,16 @@
+from typing import List
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views.generic.list import ListView
+from userprofile.models import Profile
+
+#Listar Usuários Cadastrados
+class CampoList(ListView):
+    model = Profile
+    template = "dashboard-admin/usuarios.html"
+#End Listar Usuários Cadastrados
+
 
 
 #DASHBOARD HOME
