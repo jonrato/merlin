a4c0f12da5c106ec8c01274950dce88f308eaccd
Jonatas <jonatas_ferreira@usp.br>
2021-05-26 00:05:42 -0300

    add comment

--- blog_noticias/models.py
+++ blog_noticias/models.py
@@ -1,14 +1,24 @@
 from django.contrib.auth.models import User
 from django.db import models
+from autoslug import AutoSlugField
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
 
 
 class Postpost(models.Model):
     title = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from='title')
     summary = RichTextField()
     content = RichTextUploadingField()
     author = models.ForeignKey(User, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Category)
     created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
