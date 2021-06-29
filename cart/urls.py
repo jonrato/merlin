e14d604afdbcd888b738bc5ffef8f072e563a7fa
Jonatas <jonatas_ferreira@usp.br>
2021-06-29 19:42:04 -0300

    Checkout View

--- cart/urls.py
+++ cart/urls.py
@@ -1,14 +1,16 @@
-from cart.views import cart_home, cart_update
+from cart.views import cart_home, cart_update,checkout_home
 from django.urls import include, path
 
 app_name = "cart"
 
 from .views import (
     cart_home,
+    checkout_home,
     cart_update,
 )
 
 urlpatterns = [
     path('', cart_home,name='home'),
+    path('checkout/', checkout_home, name='checkout'),
     path('update/', cart_update, name='update')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from orders.models import Order
 from .models import Cart
 from django_app.models import Post_Assinaturas
 
@@ -25,3 +26,16 @@ def cart_update(request):
     request.session['cart_items']=cart_obj.products.count()
 
     return redirect("cart:home")
+
+def checkout_home(request):
+    #aqui a gente pega o carrinho
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    cart_obj=None
+    #se o carrinho acabou de ser criado, ele estará zerado
+    #ou se o carrinho existir mas não houver nada dentro
+    if cart_created or cart_obj.products.count() == 0:
+        return redirect("cart:home")
+    #aqui a order está associada ao carrinho
+    else:
+        order_obj,new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    return render(request, "carts/checkout.html", {"object":order_obj})
