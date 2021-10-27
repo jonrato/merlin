993db217bc2ac7f4f7086556097c218268a2d5af
jonrato <jonatas_ferreira@usp.br>
2021-10-27 00:59:52 -0300

    upload-analises

--- analises/models.py
+++ analises/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+import os
 
-# Create your models here.
+def file_path(instance, filename):
+    path="documents/"
+    format = 'uploaded-' + filename
+    return os.path.join(path, format)
+
+class FileHandler(models.Model):
+
+    file_upload = models.FileField(upload_to=file_path)
+    criado = models.DateTimeField(auto_now_add=True, verbose_name="Data de Criação", blank=True, null=True)
+    name = models.CharField(max_length=50)
+    resume = RichTextField(null=True, blank=True)
+    
+    def __str__(self):
+        return str(self.file_upload)
