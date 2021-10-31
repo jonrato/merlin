1b6019b2e2ad4cf96cdc272433c9aa34d5200b26
jonrato <jonatas_ferreira@usp.br>
2021-10-31 15:23:42 -0300

    exclude bootstrap

--- django_project/settings.py
+++ django_project/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #'bootstrapform',
-    'bootstrap',
+    #'bootstrap',
     
     'crispy_forms',
     'django_app',
